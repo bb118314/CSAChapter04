fde477ef97cb5dcf2b56cc9c85add29e9139fa3d
Benjamen Brooks <bb118314@students.davenport.k12.ia.us>
2019-01-28 14:24:19 -0600

    Started on DemoBlock

---
+++ src/DemoBlock.java
@@ -0,0 +1,7 @@
+public class DemoBlock {
+    public static void main(String[] args){
+        System.out.println("Demonstrating block scope");
+        int x = 1111;
+        System.out.println("In first block x is " + x);
+    }
+}
